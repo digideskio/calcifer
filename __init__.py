238c9b81baec74edbdcc056d3f52ad1033396368
g. nicholas d'andrea <gnidan@dramafever.com>
2016-04-18 22:59:05 -0400

    Cleanup imports in policy/__init__.py

--- __init__.py
+++ __init__.py
@@ -10,9 +10,28 @@ template generation. The operators are designed to provide flexible
 tooling for the creation of high-level policy rules.
 """
 from dramafever.premium.services.policy.operators import (
-    set_value, define_as, select, path, set_path, with_value,
-    check, policies, regarding, given, fail, match, attempt,
-    permit_values
+    attempt,
+    check,
+    define_as,
+    fail,
+    get_node,
+    get_value,
+    given,
+    match,
+    path,
+    permit_values,
+    policies,
+    pop_context,
+    push_context,
+    regarding,
+    scope,
+    select,
+    set_path,
+    set_value,
+    unit,
+    unit_value,
+    with_value,
+    wrap_context,
 )
 from dramafever.premium.services.policy.partial import Partial
 from dramafever.premium.services.policy.monads import (
